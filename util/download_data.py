177b5753e41a7b58fb365649abd28d512362159e
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-27 13:50:42 +0900

    download to data folder

--- util/download_data.py
+++ util/download_data.py
@@ -1,22 +1,22 @@
 from sys import argv
 from urllib.request import urlopen
 
-
 def main():
     months = [str(x).zfill(2) for x in range(1,13)]
     years = [str(x) for x in range(2012, 2017)]
     rooturl = 'http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/'
     rootname = 'wdvc16_'
     ext = '.xml.7z'
-    if len(argv) > 1:
-        _, rooturl, ext = argv
+    directory = '../data/'
+    if len(argv) == 5:
+        _, rooturl, rootname, ext, directory = argv
     for year in years:
         for month in months:
             request_url = rooturl + year + '_' + month + ext
             try:
                 with urlopen(request_url) as response:
                   data = response.read()
-                with open(rootname+year+'_'+month+ext, 'wb') as f:
+                with open(directory+rootname+year+'_'+month+ext, 'wb') as f:
                     f.write(data)
                     print('Write ' + rootname+year+'_'+month+ext, 'wb')
             except:
